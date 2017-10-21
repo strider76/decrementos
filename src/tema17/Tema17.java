54dca53d16b33c6732a4f57384fc53aa30f4a8b7
Manuel Millán <manuel@betangible.com>
2017-10-21 13:13:57 +0200

    primer commit - ejemplo decrementos

---
+++ src/tema17/Tema17.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema17;
+
+/**
+ *
+ * @author manolo
+ */
+public class Tema17 {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Le pasamos a funcion A de 5: ");
+        Incrementos valorIncrementos = new Incrementos();
+        System.out.println("El valor final es "+ valorIncrementos.a(5));
+        
+    }
+    
+}
