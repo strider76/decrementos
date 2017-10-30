26408c1025726adecb09fc5b1b432eec026d4a9d
Manuel Millán <manuel@betangible.com>
2017-10-30 09:52:48 +0100

    Segundo commit - Version en una sola clase

--- src/tema17/Tema17.java
+++ src/tema17/Tema17.java
@@ -14,12 +14,31 @@ public class Tema17 {
     /**
      * @param args the command line arguments
      */
-
+    public Tema17() {
+        
+    }
+    
+    private int a (int valorA) {
+        int valorFuncionB = b(valorA);
+        return valorFuncionB;
+        
+    }
+    
+    private int b (int valorB) {
+        int valorDecrementado = valorB -1;
+        int valorFuncionC = c(valorDecrementado);
+        return valorFuncionC;
+    }
+    
+    private int c (int valorC) {
+        int valorDecrementado = valorC -1;
+        return valorDecrementado;
+    }  
     
     public static void main(String[] args) {
         
         System.out.println("Le pasamos a funcion A de 5: ");
-        Incrementos valorIncrementos = new Incrementos();
+        Tema17 valorIncrementos = new Tema17();
         System.out.println("El valor final es "+ valorIncrementos.a(5));
         
     }
